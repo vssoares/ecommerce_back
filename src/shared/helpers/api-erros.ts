6e2642ea207be03ca759869bc3564e9d9802ffea
vssoares <vsss23@hotmail.com>
2023-06-21 23:08:26 -0300

    custom error

--- src/shared/helpers/api-erros.ts
+++ src/shared/helpers/api-erros.ts
@@ -2,20 +2,32 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 
 const Default = 'Ocorreu um erro interno';
 
-export class BadRequestError extends HttpException {
-  constructor(message: string = Default) {
-    super({ status: false, message: message }, HttpStatus.BAD_REQUEST);
+export class CustomError extends HttpException {
+  constructor(status: HttpStatus, message: string = Default) {
+    super({ status: false, message: message }, status);
   }
 }
 
-export class NotFoundError extends HttpException {
+export class BadRequestError extends CustomError {
   constructor(message: string = Default) {
-    super({ status: false, message: message }, HttpStatus.NOT_FOUND);
+    super(HttpStatus.BAD_REQUEST, message);
   }
 }
 
-export class UnauthorizedError extends HttpException {
+export class NotFoundError extends CustomError {
   constructor(message: string = Default) {
-    super({ status: false, message: message }, HttpStatus.UNAUTHORIZED);
+    super(HttpStatus.NOT_FOUND, message);
+  }
+}
+
+export class UnauthorizedError extends CustomError {
+  constructor(message: string = Default) {
+    super(HttpStatus.UNAUTHORIZED, message);
+  }
+}
+
+export class ForbiddenError extends CustomError {
+  constructor(message: string = Default) {
+    super(HttpStatus.FORBIDDEN, message);
   }
 }
