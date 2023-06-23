058f83bb268c4f4db7ca2d00c961f6a8fe3ce191
vssoares23 <vsss23@hotmail.com>
2023-06-22 22:16:12 -0300

    ajustado validacao add carrinho

---
+++ prisma/migrations/20230623005054_init/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "CarrinhoItem" ADD COLUMN "preco_total" REAL;
--- src/ecommerce/carrinho/carrinho.service.ts
+++ src/ecommerce/carrinho/carrinho.service.ts
@@ -62,9 +62,21 @@ export class CarrinhoService {
     quantidade = +quantidade;
     produto_id = +produto_id;
     carrinho_id = +carrinho_id;
-
+    let preco;
     // Adiciona o produto ao carrinho se não existir, ou atualiza a quantidade somando com a nova
-    const produto = await this.produtoService.getProduto(produto_id);
+    const carrinhoItem = await this.getProdutoCarrinho({
+      produto_id,
+      carrinho_id,
+    });
+
+    if (!carrinhoItem) {
+      const produto = await this.produtoService.getProduto(produto_id);
+      preco = produto.preco;
+    } else {
+      preco = carrinhoItem?.preco_unitario;
+      quantidade = carrinhoItem?.quantidade + quantidade;
+    }
+
     await this.db.carrinhoItem.upsert({
       where: {
         carrinho_id_produto_id: { produto_id, carrinho_id },
@@ -73,10 +85,12 @@ export class CarrinhoService {
         produto_id,
         carrinho_id,
         quantidade,
-        preco_unitario: produto.preco,
+        preco_unitario: preco,
+        preco_total: preco * quantidade,
       },
       update: {
-        quantidade: { increment: quantidade },
+        quantidade,
+        preco_total: preco * quantidade,
       },
     });
     return await this.updateCarrinhoValor(carrinho_id);
@@ -85,10 +99,11 @@ export class CarrinhoService {
   async removerProdutoCarrinho({ produto_id, carrinho_id }) {
     // Valida se o produto e o carrinho existem
     // Remove o produto do carrinho
-    const carrinho = await this.validarProdutoCarrinho(produto_id, carrinho_id);
-    const quantidadeAtual = carrinho.itens.find(
-      (item) => item.produto_id === +produto_id,
-    ).quantidade;
+    const carrinhoItem = await this.validarProdutoCarrinho(
+      produto_id,
+      carrinho_id,
+    );
+    const quantidadeAtual = carrinhoItem.quantidade;
     if (quantidadeAtual === 1) {
       return await this.removerProdutoCarrinhoTudo({ produto_id, carrinho_id });
     }
@@ -133,15 +148,26 @@ export class CarrinhoService {
     return await this.updateCarrinhoValor(carrinho_id);
   }
 
+  async getProdutoCarrinho({ produto_id, carrinho_id }) {
+    const carrinhoItem = await this.db.carrinhoItem.findUnique({
+      where: {
+        carrinho_id_produto_id: {
+          carrinho_id,
+          produto_id,
+        },
+      },
+      include: {
+        produto: true,
+      },
+    });
+    return carrinhoItem;
+  }
+
   async validarProdutoCarrinho(produto_id, carrinho_id) {
-    await this.produtoService.getProduto(produto_id);
-    const carrinho = await this.getCarrinho({ id: carrinho_id });
-    const carrinhoItem = carrinho.itens.find(
-      (produto) => produto.produto_id === +produto_id,
-    );
+    const carrinhoItem = this.getProdutoCarrinho({ produto_id, carrinho_id });
     if (!carrinhoItem) {
       throw new NotFoundError('Produto não encontrado no carrinho!');
     }
-    return carrinho;
+    return carrinhoItem;
   }
 }
